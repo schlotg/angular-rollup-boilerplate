079077b65ec6097a889d84b73a1bce487e7c63b4
greg <greg@root-2.com>
2016-11-21 15:05:38 -0800

    added the map so you can debug

--- utils/cleanup.ts
+++ utils/cleanup.ts
@@ -3,4 +3,8 @@ const jetpack = require('fs-jetpack');
 // cleanup unnecessary files
 jetpack.find('dist', {
     matching: [ '*es2015*']
-}).forEach(jetpack.remove);
+}).forEach((file) => {
+  if (file.indexOf('map') === -1) {
+    jetpack.remove(file);
+  }
+});
